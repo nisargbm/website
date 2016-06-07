1f8ecfe4efbb81dc4069f16f8e0479852d1aecfb
nisargbm <nisargbm.vjti@gmail.com>
2016-06-07 16:49:39 +0530

    Create testprofile.php

---
+++ testprofile.php
@@ -0,0 +1,59 @@
+<div id="fb-root"></div>
+<script>(function(d, s, id) {
+  var js, fjs = d.getElementsByTagName(s)[0];
+  if (d.getElementById(id)) return;
+  js = d.createElement(s); js.id = id;
+  js.src = "//connect.facebook.net/fr_FR/all.js#xfbml=1";
+  fjs.parentNode.insertBefore(js, fjs);
+}(document, 'script', 'facebook-jssdk'));</script>
+<head>
+<link href="/styleProfile.css" rel="stylesheet" type="text/css" />
+</head>
+<body>
+
+
+
+<p class="title">Profile </p>
+
+<div class="my-profile">
+
+<img src="http://www.broumi.com/freebies/images/my-picture.png" class="profile-picture"/>
+<div class="bloc">
+<div class="content">
+<span class="name">Real Madrid</span>
+<h1 class="fonction">Madrid</h1>
+
+<p class="presentation">
+Name:</br>
+ Phone Number:</br>
+ Address:</br>
+ Age:</br>
+</p>
+
+</div>
+ <ul class="social-media">
+
+<a href="#" target="_blank">
+<li style="border-left:3px solid #2b4272;background:#3b5998;">
+Change Password:</li></a>
+
+<a href="#" target="_blank">
+<li style="border-left:3px solid #0088bb;background: #00aced;">
+Edit profile:</li></a>
+
+<a href="#" target="_blank">
+<li style="border-left:3px solid #cf4b7b;background: #f85993;">
+Home</li></a>
+
+<a href="#" target="_blank">
+<li style="border-left:3px solid #149f76;background: #1bce99;">
+About
+</li></a>
+</ul>
+
+</div>
+</div>
+
+
+</body>
+</html>
